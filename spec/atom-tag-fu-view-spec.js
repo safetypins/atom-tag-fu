9a7c24bd8dcad8c3b56dd9871c03387c97d5bbb6
Matthew Cowgur <hello@mattcowgur.com>
2017-10-25 15:24:31 -0500

    Use the atom package-generator default code.

---
+++ spec/atom-tag-fu-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomTagFuView from '../lib/atom-tag-fu-view';
+
+describe('AtomTagFuView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
